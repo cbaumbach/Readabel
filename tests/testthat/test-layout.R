702544a61ec5513603db4901ea15868612d19658
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-02-16 17:54:38 +0100

    Refactor test cases.

--- tests/testthat/test-layout.R
+++ tests/testthat/test-layout.R
@@ -2,18 +2,19 @@ fake_layout_file <- file.path(tempdir(), "fake_layout_file.iout")
 create_fake_layout_file(fake_layout_file)
 x <- read_omicabel(fake_layout_file)
 
+snp_labels <- paste0("snp", 1:9)
+trait_labels <- paste0("trait", 1:7)
+covariates <- c("intercept", "sex", "snp")
+beta_labels <- paste0("beta_", covariates)
+se_labels <- paste0("se_", covariates)
+cov_labels <- paste0("cov_", c("sex_sex", "sex_snp", "snp_snp"))
+
 test_that("names", {
-    column_names <- c("trait", "snp",
-        "beta_intercept", "beta_sex", "beta_snp",
-        "se_intercept", "se_sex", "se_snp",
-        "cov_sex_sex", "cov_sex_snp", "cov_snp_snp")
-    expect_equal(names(x), column_names)
+    expect_equal(names(x), c("trait", "snp", beta_labels, se_labels, cov_labels))
 })
 
 test_that("nrow", {
-    number_of_snps <- 9L
-    number_of_traits <- 7L
-    expect_equal(nrow(x), number_of_snps * number_of_traits)
+    expect_equal(nrow(x), length(snp_labels) * length(trait_labels))
 })
 
 test_that("ncol", {
@@ -25,9 +26,9 @@ test_that("dim", {
 })
 
 test_that("snpNames", {
-    expect_equal(snpNames(x), paste0("snp", 1:9))
+    expect_equal(snpNames(x), snp_labels)
 })
 
 test_that("traitNames", {
-    expect_equal(traitNames(x), paste0("trait", 1:7))
+    expect_equal(traitNames(x), trait_labels)
 })
