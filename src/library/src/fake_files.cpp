b1144229f52ea14b1114630aacd800f188fad0cb
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-02-22 17:48:08 +0100

    Refactor Readabel::create_fake_layout_file.cpp.

--- src/library/src/fake_files.cpp
+++ src/library/src/fake_files.cpp
@@ -4,22 +4,28 @@
 
 #define NELEMS(x) (sizeof(x) / sizeof((x)[0]))
 
-static const int BYTES_PER_LABEL = 20;
+static const int magic_number = 6;
+static const int bytes_per_double = sizeof(double);
+static const int number_of_variables = 3;  // intercept, snp, sex
+static const int number_of_snps = 9;
+static const int number_of_traits = 7;
+static const int snps_per_tile = 4;
+static const int traits_per_tile = 3;
+static const int bytes_per_label = 20;
 
 void Readabel::create_fake_layout_file(std::string filename)
 {
-    FILE *fp;
-    int i, buffer[] = {
-        6,  // magic number
-        8,  // bytes per double
-        3,  // number of variables: intercept, snp, sex
-        9,  // number of snps
-        7,  // number of traits
-        4,  // snps per tile
-        3,  // traits per tile
-        BYTES_PER_LABEL
-    };
-    char labels[][BYTES_PER_LABEL] = {
+    int buffer[8];
+    buffer[0] = magic_number;
+    buffer[1] = bytes_per_double;
+    buffer[2] = number_of_variables;
+    buffer[3] = number_of_snps;
+    buffer[4] = number_of_traits;
+    buffer[5] = snps_per_tile;
+    buffer[6] = traits_per_tile;
+    buffer[7] = bytes_per_label;
+
+    char labels[][bytes_per_label] = {
         "beta_intercept", "beta_sex", "beta_snp",                                // betas
         "se_intercept", "se_sex", "se_snp",                                      // standard errors
         "cov_sex_sex", "cov_sex_snp", "cov_snp_snp",                             // covariances
@@ -27,12 +33,13 @@ void Readabel::create_fake_layout_file(std::string filename)
         "trait1", "trait2", "trait3", "trait4", "trait5", "trait6", "trait7",    // traits
     };
 
+    FILE *fp;
     if ((fp = fopen(filename.c_str(), "wb")) == NULL)
         return;
 
     fwrite((void *) buffer, sizeof(buffer), 1, fp);
-    for (i = 0; i < (int) NELEMS(labels); i++)
-        fwrite((void *) labels[i], BYTES_PER_LABEL, 1, fp);
+    for (int i = 0; i < (int) NELEMS(labels); i++)
+        fwrite((void *) labels[i], bytes_per_label, 1, fp);
 
     fclose(fp);
 }
