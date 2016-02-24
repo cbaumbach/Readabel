500aca3b28031c6dd855d5b21070c5811e872ad6
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-02-24 15:03:46 +0100

    Use PACKAGE argument of .Call interface.

--- R/readabel-class.R
+++ R/readabel-class.R
@@ -11,7 +11,7 @@ setGeneric("names")
 #' @param x An object of class Readabel.
 #' @export
 setMethod("names", "Readabel", function(x) {
-    .Call("rcpp_names", x@pointer)
+    .Call("rcpp_names", x@pointer, PACKAGE = "readabel")
 })
 
 #' Return a new Readabel object.
@@ -19,7 +19,7 @@ setMethod("names", "Readabel", function(x) {
 #' @param x An uninitialized object of class Readabel
 #' @param filename The path to a binary OmicABEL output file
 setMethod("initialize", "Readabel", function(.Object, filename) {
-    .Object@pointer <- .Call("rcpp_new", filename)
+    .Object@pointer <- .Call("rcpp_new", filename, PACKAGE = "readabel")
     .Object
 })
 
@@ -38,7 +38,7 @@ setGeneric("nrow")
 #' @param x An object of class Readabel
 #' @export
 setMethod("nrow", "Readabel", function(x) {
-    .Call("rcpp_nrow", x@pointer)
+    .Call("rcpp_nrow", x@pointer, PACKAGE = "readabel")
 })
 
 setGeneric("ncol")
@@ -68,7 +68,7 @@ setGeneric("snpNames", function(x) standardGeneric("snpNames"))
 #' @param x An object of class Readabel
 #' @export
 setMethod("snpNames", "Readabel", function(x) {
-    .Call("rcpp_snpNames", x@pointer)
+    .Call("rcpp_snpNames", x@pointer, PACKAGE = "readabel")
 })
 
 setGeneric("traitNames", function(x) standardGeneric("traitNames"))
@@ -78,5 +78,5 @@ setGeneric("traitNames", function(x) standardGeneric("traitNames"))
 #' @param x An object of class Readabel
 #' @export
 setMethod("traitNames", "Readabel", function(x) {
-    .Call("rcpp_traitNames", x@pointer)
+    .Call("rcpp_traitNames", x@pointer, PACKAGE = "readabel")
 })
