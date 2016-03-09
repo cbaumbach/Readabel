5249e123b6e0dfa8f550f8cc20f0fe2b208a15cd
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-03-09 22:39:10 +0100

    Use single quotes in tests for better readability.

--- tests/testthat/test-sub.R
+++ tests/testthat/test-sub.R
@@ -2,12 +2,12 @@ source("setup.R")
 
 context("[")
 
-test_that("x[, \"snp\", drop = FALSE]", {
+test_that("x[, 'snp', drop = FALSE]", {
     expect_equal(d[, "snp", drop = FALSE],
                  x[, "snp", drop = FALSE])
 })
 
-test_that("x[, c(\"snp\", \"beta_snp\"), drop = FALSE]", {
+test_that("x[, c('snp', 'beta_snp'), drop = FALSE]", {
     expect_equal(d[, c("snp", "beta_snp"), drop = FALSE],
                  x[, c("snp", "beta_snp"), drop = FALSE])
 })
