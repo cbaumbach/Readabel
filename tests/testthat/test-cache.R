7587b2eca3f9a871790078872f44ea1157097860
Clemens Baumbach <clemens.baumbach@gmx.net>
2016-03-14 11:15:29 +0100

    Caching fails when multiple columns are extracted.

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -9,3 +9,13 @@ test_that("caching works with [[", {
     x[[column]]
     expect_true(is_in_cache(x, column))
 })
+
+test_that("caching works when more than one column is extracted", {
+    clear_cache(x)
+    columns <- c("beta_sex", "snp")
+    for (col in columns)
+        expect_false(is_in_cache(x, col))
+    x[columns]
+    for (col in columns)
+        expect_true(is_in_cache(x, col))
+})
